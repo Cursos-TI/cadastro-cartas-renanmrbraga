e7e893b9a8706e40258e0b0b0eb854d45e6aa5a5
Renan <renanmbraga@outlook.com>
2026-05-24 13:17:28 -0300

    feat: adiciona menu interativo com switch e comparacoes aninhadas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,39 +2,15 @@
 
 int main()
 {
-    /*
-    Carta 1:
-    Estado: SP
-    Código: A01
-    Nome da Cidade: São Paulo
-    População: 12325000
-    Área: 1521.11 km²
-    PIB: 699.28 bilhões de reais
-    Número de Pontos Turísticos: 50
-
-    Carta 2:
-    Estado: RJ
-    Código: B02
-    Nome da Cidade: Rio de Janeiro
-    População: 6748000
-    Área: 1200.25 km²
-    PIB: 300.50 bilhões de reais
-    Número de Pontos Turísticos: 30
-    */
-
     // Declaração das variáveis em snake case
     unsigned long int populacao_1, populacao_2;
     float area_1, area_2, pib_1, pib_2;
-    int pontos_turisticos_1, pontos_turisticos_2;
-    char estado_1[3], estado_2[3], codigo_1[4], codigo_2[4], cidade_1[50], cidade_2[50];
+    int opcao, pontos_turisticos_1, pontos_turisticos_2;
+    char pais_1[40], pais_2[40];
 
     // Entrada de dados da Carta 1
-    printf("Digite o Estado 1 (sigla, ex: SP): ");
-    scanf("%s", estado_1);
-    printf("Digite o Codigo 1 (ex: A01): ");
-    scanf("%s", codigo_1);
-    printf("Digite o Nome da Cidade 1: ");
-    scanf(" %[^\n]", cidade_1);
+    printf("Digite o nome do País 1: ");
+    scanf(" %[^\n]", pais_1);
     printf("Digite a Populacao 1: ");
     scanf("%lu", &populacao_1);
     printf("Digite a Area 1: ");
@@ -45,12 +21,8 @@ int main()
     scanf("%d", &pontos_turisticos_1);
 
     // Entrada de dados da Carta 2
-    printf("\nDigite o Estado 2 (sigla, ex: RJ): ");
-    scanf("%s", estado_2);
-    printf("Digite o Codigo 2 (ex: B02): ");
-    scanf("%s", codigo_2);
-    printf("Digite o Nome da Cidade 2: ");
-    scanf(" %[^\n]", cidade_2);
+    printf("Digite o nome do País 2: ");
+    scanf(" %[^\n]", pais_2);
     printf("Digite a Populacao 2: ");
     scanf("%lu", &populacao_2);
     printf("Digite a Area 2: ");
@@ -62,26 +34,155 @@ int main()
 
     // Cálculos de Densidade e PIB per Capita
     float densidade_1 = (float)populacao_1 / area_1;
-    float pib_capita_1 = (pib_1 * 1e9) / (float)populacao_1;
+    float pib_per_capita_1 = (pib_1 * 1e9) / (float)populacao_1;
     float densidade_2 = (float)populacao_2 / area_2;
-    float pib_capita_2 = (pib_2 * 1e9) / (float)populacao_2;
+    float pib_per_capita_2 = (pib_2 * 1e9) / (float)populacao_2;
 
-    // Exibição dos dados de comparação
-    printf("\nCarta 1 - %s (%s) %.2f\n", cidade_1, estado_1, densidade_1);
-    printf("Carta 2 - %s (%s) %.2f\n", cidade_2, estado_2, densidade_2);
+    // Exibição do menu interativo para escolha do atributo
+    printf("Escolha uma opção para comparar:\n");
+    printf("1. População\n");
+    printf("2. Área\n");
+    printf("3. PIB\n");
+    printf("4. Pontos Turísticos\n");
+    printf("5. Densidade\n");
+    scanf("%d", &opcao);
 
-    // Condição de vencedor utilizando os dados de comparação
-    if (densidade_1 < densidade_2)
+    // Impressão dos nomes dos países para contexto do jogador
+    printf("País 1: %s\n", pais_1);
+    printf("País 2: %s\n", pais_2);
+
+    // Estrutura de decisão para comparar a opção selecionada no menu
+    switch (opcao)
     {
-        printf("Resultado: Carta 1 (%s) venceu!\n", cidade_1);
-    }
-    else if (densidade_1 > densidade_2)
-    {
-        printf("Resultado: Carta 2 (%s) venceu!\n", cidade_2);
-    }
-    else
-    {
-        printf("Resultado: Empate!\n");
+    case 1:
+        // Comparação baseada no atributo População
+        printf("Atributo: População\n");
+        printf("Carta 1 (%s): %lu\n", pais_1, populacao_1);
+        printf("Carta 2 (%s): %lu\n", pais_2, populacao_2);
+
+        // Estrutura condicional aninhada para verificar empate ou vencedor
+        if (populacao_1 == populacao_2)
+        {
+            printf("Empate!\n");
+        }
+        else
+        {
+            // Regra padrão: Maior valor vence
+            if (populacao_1 > populacao_2)
+            {
+                printf("Carta 1 (%s) venceu!\n", pais_1);
+            }
+            else
+            {
+                printf("Carta 2 (%s) venceu!\n", pais_2);
+            }
+        }
+        break;
+
+    case 2:
+        // Comparação baseada no atributo Área
+        printf("Atributo: Área\n");
+        printf("Carta 1 (%s): %.2f\n", pais_1, area_1);
+        printf("Carta 2 (%s): %.2f\n", pais_2, area_2);
+
+        // Estrutura condicional aninhada para verificar empate ou vencedor
+        if (area_1 == area_2)
+        {
+            printf("Empate!\n");
+        }
+        else
+        {
+            // Regra padrão: Maior valor vence
+            if (area_1 > area_2)
+            {
+                printf("Carta 1 (%s) venceu!\n", pais_1);
+            }
+            else
+            {
+                printf("Carta 2 (%s) venceu!\n", pais_2);
+            }
+        }
+        break;
+
+    case 3:
+        // Comparação baseada no atributo PIB
+        printf("Atributo: PIB\n");
+        printf("Carta 1 (%s): %.2f\n", pais_1, pib_1);
+        printf("Carta 2 (%s): %.2f\n", pais_2, pib_2);
+
+        // Estrutura condicional aninhada para verificar empate ou vencedor
+        if (pib_1 == pib_2)
+        {
+            printf("Empate!\n");
+        }
+        else
+        {
+            // Regra padrão: Maior valor vence
+            if (pib_1 > pib_2)
+            {
+                printf("Carta 1 (%s) venceu!\n", pais_1);
+            }
+            else
+            {
+                printf("Carta 2 (%s) venceu!\n", pais_2);
+            }
+        }
+        break;
+
+    case 4:
+        // Comparação baseada no atributo Pontos Turísticos
+        printf("Atributo: Pontos Turísticos\n");
+        printf("Carta 1 (%s): %d\n", pais_1, pontos_turisticos_1);
+        printf("Carta 2 (%s): %d\n", pais_2, pontos_turisticos_2);
+
+        // Estrutura condicional aninhada para verificar empate ou vencedor
+        if (pontos_turisticos_1 == pontos_turisticos_2)
+        {
+            printf("Empate!\n");
+        }
+        else
+        {
+            // Regra padrão: Maior valor vence
+            if (pontos_turisticos_1 > pontos_turisticos_2)
+            {
+                printf("Carta 1 (%s) venceu!\n", pais_1);
+            }
+            else
+            {
+                printf("Carta 2 (%s) venceu!\n", pais_2);
+            }
+        }
+        break;
+
+    case 5:
+        // Comparação baseada no atributo Densidade Populacional
+        printf("Atributo: Densidade\n");
+        printf("Carta 1 (%s): %.2f\n", pais_1, densidade_1);
+        printf("Carta 2 (%s): %.2f\n", pais_2, densidade_2);
+
+        // Estrutura condicional aninhada para verificar empate ou vencedor
+        if (densidade_1 == densidade_2)
+        {
+            printf("Empate!\n");
+        }
+        else
+        {
+            // Regra especial: Menor valor vence
+            if (densidade_1 < densidade_2)
+            {
+                printf("Carta 1 (%s) venceu!\n", pais_1);
+            }
+            else
+            {
+                printf("Carta 2 (%s) venceu!\n", pais_2);
+            }
+        }
+        break;
+
+    default:
+        // Tratamento de segurança para opções inexistentes no menu
+        printf("Opção inválida.\n");
+        break;
     }
 
     return 0;
